112c5891c82a4c140508c78c7e3e3f7de72395db
Aldofig95 <aldof95@live.com>
2024-11-15 14:12:51 -0600

    Throwing exceptions test

---
+++ NSubstituteTest.Tests/ThrowException.cs
@@ -0,0 +1,23 @@
+﻿using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSubstituteTest.Tests
+{
+    public class ExceptionTests
+    {
+        [Fact]
+        public void DoWork_ShouldThrowInvalidOperationException()
+        {
+            var myService = Substitute.For<IMyService>();
+            myService.When(x => x.DoWork()).Do(x => { throw new InvalidOperationException("Error happened !!"); });
+
+            var exception = Assert.Throws<InvalidOperationException>(() => myService.DoWork());
+            //If msj is different, we'll get the results
+            Assert.Equal("Error happened !!", exception.Message);
+        }
+    }
+}
