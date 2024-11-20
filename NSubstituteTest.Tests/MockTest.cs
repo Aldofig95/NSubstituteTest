119397e3cfd686a69a6e6b6bb3b60f7dfaef4c9b
Aldofig95 <aldof95@live.com>
2024-11-20 16:07:08 -0600

    Service mocking test, forgot to upload them yesterday

---
+++ NSubstituteTest.Tests/MockTest.cs
@@ -0,0 +1,20 @@
+﻿using NSubstitute;
+using NSubstituteTest;
+using Xunit;
+
+namespace NSubstituteTest.Tests
+{
+    public class MockTest
+    {
+        [Fact]
+        public void GetMockInfo()
+        {
+            var mockService = Substitute.For<IMockService>();
+            mockService.GetData().Returns("Mocked Data test");
+            var controller = new MockController(mockService);
+            var result = controller.GetInfo();
+
+            Assert.Equal("Mocked Data test", result);
+        }
+    }
+}
---
+++ NSubstituteTest/IMockService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace NSubstituteTest
+{
+    public interface IMockService
+    {
+        string GetData();
+    }
+
+    public class MockController
+    {
+        private readonly IMockService _service;
+
+        public MockController(IMockService service)
+        {
+            _service = service;
+        }
+
+        public string GetInfo()
+        {
+            return _service.GetData();
+        }
+    }
+}
+
